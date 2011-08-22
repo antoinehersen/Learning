2180f7b5a593db0182961d64e81b57eafa8aeb10
Antoine Hersen <antoine.hersen@gmail.com>
2011-08-22 17:31:19 -0500

    Strating chap 1.2

--- sicp/chap1.scm
+++ sicp/chap1.scm
@@ -24,3 +24,68 @@
 
 (define (improve-guess guess x)
   (/ (+ (/ x guess) guess) 2))
+
+(define (square x)
+  (* x x))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+;; block structure definition
+(define (sqrt x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+  (sqrt-iter 1.0 x))
+
+;; We can get rid of the x since it is already in scope
+
+(define (sqrt-nox x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+
+
+
+;; 1.2  Procedures and the Processes They Generate
+
+(define (fact x)
+(define (fact-iter prod curr)
+  (if (> curr x)
+      prod
+      (fact-iter (* prod curr) (+ 1 curr))))
+(fact-iter 1 1))
+
+
+;; Ackerman function
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+;;  f(n) = n if n<3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n> 3.
+
+(define (f n)
+  (if (< n 3)
+      n
+      (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
+
+(define (f2 n)
+(define (f-iter a b c count)
+  (if (= count 2 )
+      c
+      (f-iter b c (+ c (* 2 b) (* 3 a)) (- count 1))))
+  (f-iter 0 1 2 n))
